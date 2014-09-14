a00eb410d20136a0debf87d1b89409050fef81a7
Bijender <bijenderk82@gmail.com>
2014-09-14 16:43:22 -0400

    add new files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,15 @@
 angular.module('yeomanAngularApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [
-      'item1','item2','item3'
+      'item1','item2','item4'
     ];
+
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    
+    $scope.removeTodo = function(index){
+    	$scope.todos.splice(index,1);
+    }
   });
